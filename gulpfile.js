2036c38fd7eed3f013e7ed8174e681b8c7628b40
zmadou <ferreira1337@hotmail.com>
2024-07-27 21:40:53 -0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require("gulp");
+const sass = require('gulp-sass')(require('sass'));
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+
+
+function comprimeIMG() {
+    return gulp.src('./source/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./build/images'))
+}
+function comprimeJS() {
+    return gulp.src('./source/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./build/scripts'))
+}
+
+function compiladorSass() {
+    return gulp.src('./source/styles/main.scss')
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(gulp.dest('./build/styles'))
+}
+
+
+
+exports.default = function() {
+    gulp.watch('./source/styles/*.scss', { ignoreInitial: false }, gulp.series(compiladorSass));
+    gulp.watch('./source/scripts/*.js', { ignoreInitial: false }, gulp.series(comprimeJS));
+    gulp.watch('./source/images/*', { ignoreInitial: false }, gulp.series(comprimeIMG));
+}
---
+++ source/scripts/contaPercentFalta.js
@@ -0,0 +1,4 @@
+function calculoPercentFalta(cargaHorariaFalta, cargaHorariaTotal) {
+    return (cargaHorariaFalta * 100) / cargaHorariaTotal;
+}
+console.log(calculoPercentFalta(3.33, 120));
